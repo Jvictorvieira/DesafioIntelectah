935c9fc9c5f676e4acd2bb63e76fb44d9c58b54c
João Victor Vieira <joaoovictor.21@gmail.com>
2025-08-15 17:15:46 -0300

    feat: Atualiza ViewModel do usuario logado

--- ConcessionariaAPP/Models/UserViewModel/LoggedUserViewModel.cs
+++ ConcessionariaAPP/Models/UserViewModel/LoggedUserViewModel.cs
@@ -1,7 +1,9 @@
+using ConcessionariaAPP.Domain.Enum;
+
 namespace ConcessionariaAPP.Models.UserViewModel;
 
 public class LoggedUserViewModel
 {
     public string? UserName { get; set; }
-    public int? AccessLevel { get; set; }
+    public AccessLevel AccessLevel { get; set; }
 }
