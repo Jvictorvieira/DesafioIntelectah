42b1b56fbe059b9199db76d8c8d3f41250c169ce
João Victor Vieira <joaoovictor.21@gmail.com>
2025-08-15 23:40:08 -0300

    feat: refatora serviço de veiculo e add automapper

--- ConcessionariaAPP/Application/Services/VehicleAppService.cs
+++ ConcessionariaAPP/Application/Services/VehicleAppService.cs
@@ -4,39 +4,83 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using ConcessionariaAPP.Application.Interfaces;
 using ConcessionariaAPP.Domain.Interfaces;
+using ConcessionariaAPP.Application.Dto;
 using ConcessionariaAPP.Domain.Entities;
-
-
+using AutoMapper;
 
 public class VehicleAppService : IVehicleService
 {
     private readonly IVehicleRepository _vehicleRepository;
-
-    public VehicleAppService(IVehicleRepository vehicleRepository)
+    private readonly IMapper _mapper;
+    public VehicleAppService(IVehicleRepository vehicleRepository, IMapper mapper)
     {
         _vehicleRepository = vehicleRepository;
+        _mapper = mapper;
     }
-    public async Task<Vehicles> CreateAsync(Vehicles entity)
+    public async Task<VehicleDto> CreateAsync(VehicleDto dto)
     {
-        return await _vehicleRepository.CreateAsync(entity);
+        Validate(dto, isUpdate: false);
+
+        var entity = _mapper.Map<Vehicles>(dto);
+        entity.IsDeleted = false; 
+
+        var created = await _vehicleRepository.CreateAsync(entity);
+        return _mapper.Map<VehicleDto>(created);
     }
+
+    
     public async Task<bool> DeleteAsync(int id)
     {
         return await _vehicleRepository.DeleteAsync(id);
     }
 
-    public async Task<IEnumerable<Vehicles>> GetAllAsync()
+    public async Task<IEnumerable<VehicleDto>> GetAllAsync()
     {
-        return await _vehicleRepository.GetAllAsync();
+        var list = await _vehicleRepository.GetAllAsync();
+        return [.. list.Select(e => _mapper.Map<VehicleDto>(e))];
     }
 
-    public async Task<Vehicles> GetByIdAsync(int id)
+    public async Task<VehicleDto> GetByIdAsync(int id)
     {
-        return await _vehicleRepository.GetByIdAsync(id);
+        var entity = await _vehicleRepository.GetByIdAsync(id);
+        return _mapper.Map<VehicleDto>(entity);
     }
-    public async Task<Vehicles> UpdateAsync(Vehicles entity)
+    public async Task<VehicleDto> UpdateAsync(VehicleDto dto)
     {
-        return await _vehicleRepository.UpdateAsync(entity);
+        Validate(dto, isUpdate: true);
+        if (dto.VehicleId.HasValue)
+        {
+            var entity = await _vehicleRepository.GetByIdAsync(dto.VehicleId.Value);
+            if (entity.IsDeleted)
+                throw new InvalidOperationException("Veículo está excluído (lógico).");
+
+            _mapper.Map(dto, entity);
+
+            var updated = await _vehicleRepository.UpdateAsync(entity);
+            return _mapper.Map<VehicleDto>(updated);
+        }
+        else
+        {
+            throw new ArgumentException("O objeto não contém um ID válido para atualização.", nameof(dto.VehicleId));
+        }
+    }
+    
+    private static void Validate(VehicleDto dto, bool isUpdate)
+    {
+        if (dto is null)
+        {
+            throw new ArgumentNullException(nameof(dto));
+        }
+        if (isUpdate && dto.VehicleId <= 0)
+        {
+            throw new ArgumentException("Id inválido para atualização.", nameof(dto.VehicleId));
+        }
+
+        if (string.IsNullOrWhiteSpace(dto.Model))
+        {
+            throw new ArgumentException("Nome do Modelo é obrigatório.", nameof(dto.Model));
+        }
+
     }
 
 }
