3e4ea1071b04f0a76e9a82ff011fe3132d0afa12
João Victor Vieira <joaoovictor.21@gmail.com>
2025-08-17 15:46:20 -0300

    feat: Configura autoMapper para as entidades

--- ConcessionariaAPP/Application/Mapping/AplicationMapper.cs
+++ ConcessionariaAPP/Application/Mapping/AplicationMapper.cs
@@ -3,8 +3,11 @@ using ConcessionariaAPP.Application.Dto;
 
 using ConcessionariaAPP.Domain.Entities;
 using ConcessionariaAPP.Models.ManufacturerViewModel;
+using ConcessionariaAPP.Models.SaleViewModel;
 using ConcessionariaAPP.Models.VehicleViewModel;
-// seus DTOs de Request/Response
+using ConcessionariaAPP.Models.CarDealershipViewModel;
+using ConcessionariaAPP.Models.ClientViewModel;
+
 
 namespace ConcessionariaAPP.Application.Mapping
 {
@@ -13,25 +16,43 @@ namespace ConcessionariaAPP.Application.Mapping
         public ApplicationMappingProfile()
         {
             CreateMap<Vehicles, VehicleDto>()
-            .ForMember(dest => dest.ManufacturerIds, opt => opt.MapFrom(src => src.Manufacturers.Select(m => m.ManufacturerId).ToList()))
-            .ForMember(dest => dest.ManufacturerNames, opt => opt.MapFrom(src => src.Manufacturers.Select(m => m.Name).ToList()));
+            .ForMember(dest => dest.ManufacturerName, opt => opt.MapFrom(src => src.Manufacturer.Name));
 
-            
             CreateMap<VehicleDto, Vehicles>()
-                .ForMember(dest => dest.Manufacturers, opt => opt.Ignore()); // Relacione manualmente no serviço/repositório
+                .ForMember(dest => dest.Manufacturer, opt => opt.Ignore()); 
 
 
             CreateMap<VehicleDto, VehicleViewModel>()
-                .ForMember(dest => dest.ManufacturerIds, opt => opt.MapFrom(src => src.ManufacturerIds))
-                .ForMember(dest => dest.ManufacturerNames, opt => opt.MapFrom(src => src.ManufacturerNames))
-                .ForMember(dest => dest.Manufacturer, opt => opt.MapFrom(src => src.ManufacturerNames != null && src.ManufacturerNames.Count > 0
-                    ? string.Join(", ", src.ManufacturerNames)
-                    : "Nenhum fabricante selecionado"))
+                .ForMember(dest => dest.ManufacturerName, opt => opt.MapFrom(src => src.ManufacturerName))
                 .ReverseMap();
 
             CreateMap<Manufacturers, ManufacturerDto>().ReverseMap();
 
             CreateMap<ManufacturerDto, ManufacturerViewModel>().ReverseMap();
+
+            CreateMap<SaleDto, SaleViewModel>()
+                .ForMember(dest => dest.ClientName, opt => opt.MapFrom(src => src.ClientName))
+                .ForMember(dest => dest.CarDealershipName, opt => opt.MapFrom(src => src.CarDealershipName))
+                .ForMember(dest => dest.VehicleModel, opt => opt.MapFrom(src => src.VehicleModel))
+                .ReverseMap();
+
+            CreateMap<Sales, SaleDto>()
+                .ForMember(dest => dest.ClientName, opt => opt.MapFrom(src => src.Client.Name))
+                .ForMember(dest => dest.CarDealershipName, opt => opt.MapFrom(src => src.CarDealership.Name))
+                .ForMember(dest => dest.VehicleModel, opt => opt.MapFrom(src => src.Vehicle.Model));
+            
+            CreateMap<SaleDto, Sales>()
+                .ForMember(dest => dest.Client, opt => opt.Ignore())
+                .ForMember(dest => dest.CarDealership, opt => opt.Ignore())
+                .ForMember(dest => dest.Vehicle, opt => opt.Ignore());
+
+            CreateMap<ClientDto, ClientViewModel>().ReverseMap();
+
+            CreateMap<Clients, ClientDto>().ReverseMap();
+
+            CreateMap<CarDealership, CarDealershipDto>().ReverseMap();
+
+            CreateMap<CarDealershipDto, CarDealershipViewModel>().ReverseMap();
 
         }
     }
