809700b8e1dae3d0e06547e6ffd5e9963e0d4933
João Victor Vieira <joaoovictor.21@gmail.com>
2025-08-17 15:45:20 -0300

    feat: Cria serviço de venda

---
+++ ConcessionariaAPP/Application/Interface/ISaleService.cs
@@ -0,0 +1,8 @@
+namespace ConcessionariaAPP.Application.Interfaces;
+
+using ConcessionariaAPP.Application.Dto;
+
+public interface ISaleService : IGenericCrudInterface<SaleDto>
+{
+
+}
---
+++ ConcessionariaAPP/Application/Services/SaleService.cs
@@ -0,0 +1,78 @@
+namespace ConcessionariaAPP.Application.Services;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ConcessionariaAPP.Application.Interfaces;
+using ConcessionariaAPP.Domain.Interfaces;
+using ConcessionariaAPP.Domain.Entities;
+using ConcessionariaAPP.Application.Dto;
+using AutoMapper;
+
+public class SaleAppService : ISaleService
+{
+    private readonly ISaleRepository _SaleRepository;
+    private readonly IMapper _mapper;
+    public SaleAppService(ISaleRepository SaleRepository, IMapper mapper)
+    {
+        _SaleRepository = SaleRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<SaleDto> CreateAsync(SaleDto dto)
+    {
+        Validate(dto, isUpdate: false);
+        var entity = _mapper.Map<Sales>(dto);
+
+        entity.IsDeleted = false;
+
+        
+
+        var created = await _SaleRepository.CreateAsync(entity);
+        return _mapper.Map<SaleDto>(created);
+    }
+
+    public async Task<SaleDto> UpdateAsync(SaleDto dto)
+    {
+        Validate(dto, isUpdate: true);
+
+        var entity = _mapper.Map<Sales>(dto);
+        var updated = await _SaleRepository.UpdateAsync(entity);
+        return _mapper.Map<SaleDto>(updated);
+    }
+
+    public async Task<SaleDto> GetByIdAsync(int id)
+    {
+        var entity = await _SaleRepository.GetByIdAsync(id);
+        return _mapper.Map<SaleDto>(entity);
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        if (id <= 0)
+        {
+            throw new ArgumentException("Id inválido para exclusão.", nameof(id));
+        }
+        return await _SaleRepository.DeleteAsync(id);
+    }
+
+    public async Task<IEnumerable<SaleDto>> GetAllAsync()
+    {
+        var list = await _SaleRepository.GetAllAsync();
+        return [.. list.Select(e => _mapper.Map<SaleDto>(e))];
+    }
+
+    private static void Validate(SaleDto dto, bool isUpdate)
+    {
+        if (dto is null)
+        {
+            throw new ArgumentNullException(nameof(dto));
+        }
+        if (isUpdate && dto.SaleId <= 0)
+        {
+            throw new ArgumentException("Id inválido para atualização.", nameof(dto.SaleId));
+        }
+
+    }
+    
+
+}
