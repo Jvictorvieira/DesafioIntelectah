396ab2646bb25a6ab4593549fbf3f816b2a2edb7
João Victor Vieira <joaoovictor.21@gmail.com>
2025-08-17 22:40:43 -0300

    feat: Add baseController

---
+++ ConcessionariaAPP/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using ConcessionariaAPP.Domain.Entities;
+using ConcessionariaAPP.Models.UserViewModel;
+using Microsoft.AspNetCore.Authorization;
+using ConcessionariaAPP.Infrastructure;
+using ConcessionariaAPP.Application.Excepetions;
+
+namespace ConcessionariaAPP.Controllers;
+
+public class BaseController : Controller
+{
+    protected void HandleException(AppValidationException ex)
+    {
+        foreach (var (field, messages) in ex.Errors)
+        {
+            var key =  field;
+            foreach (var msg in messages)
+                this.ModelState.AddModelError(key, msg);
+        }
+    }
+}
